4fc8af199cded98127610d9fca63b8dbd5920433
t-iffany <tiffanyxleong@gmail.com>
2022-11-25 07:21:20 +0000

    Refactored functions.

--- index.js
+++ index.js
@@ -18,28 +18,31 @@ function newTask(title, description) {
   // taskComplete.push(false);
 }
 
+
+// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
+function completeTask(task) {
+  task.complete = true;
+}
+
+// Print the state of a task to the console in a nice readable way
+function logTaskState(task) {
+  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
+}
+
 // DRIVER CODE BELOW
 
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
 
+logTaskState(task1); // Clean Cat Litter has not been completed
+completeTask(task1);
+logTaskState(task1); // Clean Cat Litter has been completed
+
 // for now, let's just make sure we see our tasks
 console.log(tasks);
 
 /*
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(taskIndex) {
-  taskComplete[taskIndex] = true;
-}
-
-// Print the state of a task to the console in a nice readable way
-function logTaskState(taskIndex) {
-  const title = taskTitles[taskIndex];
-  const complete = taskComplete[taskIndex];
-  console.log(`${title} has${complete ? " " : " not "}been completed`);
-}
-
 // DRIVER CODE BELOW
 
 newTask("Clean Cat Litter"); // task 0
