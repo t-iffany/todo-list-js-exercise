62d55ae11085f01066753d3c811b07acfee279f1
t-iffany <tiffanyxleong@gmail.com>
2022-11-25 07:27:30 +0000

    Turned functions into methodsand code into object oriented.

--- index.js
+++ index.js
@@ -10,45 +10,28 @@ function newTask(title, description) {
   const task = {
     title: title,
     description: description,
-    complete: false
+    complete: false,
+
+    // Print the state of a task to the console in a nice readable way
+    logState: function() {
+      console.log(`${this.title} has${this.complete ? " " : " not "}been completed`);
+    },
+
+    // Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
+    markCompleted: function() {
+      this.complete = true;
+    }
   };
   return task;
-  // taskTitles.push(title);
-  // taskDescriptions.push(description);
-  // taskComplete.push(false);
 }
 
 
-// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
-function completeTask(task) {
-  task.complete = true;
-}
-
-// Print the state of a task to the console in a nice readable way
-function logTaskState(task) {
-  console.log(`${task.title} has${task.complete ? " " : " not "}been completed`);
-}
-
-// DRIVER CODE BELOW
+// DRIVER CODE CHANGES BELOW
 
 const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
 const task2 = newTask("Do Laundry", "😨");
 const tasks = [task1, task2];
 
-logTaskState(task1); // Clean Cat Litter has not been completed
-completeTask(task1);
-logTaskState(task1); // Clean Cat Litter has been completed
-
-// for now, let's just make sure we see our tasks
-console.log(tasks);
-
-/*
-// DRIVER CODE BELOW
-
-newTask("Clean Cat Litter"); // task 0
-newTask("Do Laundry"); // task 1
-
-logTaskState(0); // Clean Cat Litter has not been completed
-completeTask(0);
-logTaskState(0); // Clean Cat Litter has been completed
-*/
+task1.logState(); // Clean Cat Litter has not been completed
+task1.markCompleted();
+task1.logState(); // Clean Cat Litter has been completed
